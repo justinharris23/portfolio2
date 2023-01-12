9a79cc0c33fbe666c17a2e7e8438f99a3df6052d
Justin Harris <justin.r.harris@gmail.com>
2023-01-11 21:09:30 -0500

    updated readme

--- jh-portfolio/.next/static/chunks/webpack.js
+++ jh-portfolio/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d7279ac0df9aa354"; }
+/******/ 		__webpack_require__.h = function() { return "e9964412a40b1aea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
